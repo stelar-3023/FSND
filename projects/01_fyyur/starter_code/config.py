ff2b3adeda9b12d302b2b4df26bec973e5c91d26
Steve Larsen <slarsen-3@att.net>
2020-11-09 16:36:13 -0600

    updated app.py / config .py

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,5 +9,6 @@ DEBUG = True
 # Connect to the database
 
 
+
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:H3rcul3$@localhost:5432/fyyurproject'
